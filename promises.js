e36aee3c7a5e7610b52dd827916c55808a1e0139
MSI\moneo <jvr.moneo@gmail.com>
2020-10-13 12:12:07 +0200

    metodos 1

--- promises.js
+++ promises.js
@@ -75,42 +75,75 @@
 // console.log('Fin del proceso');
 
 //sintaxis 3
-console.log('Iniciando proceso de subida');
+// console.log('Iniciando proceso de subida');
 
-function photoUpload() {
+// function photoUpload() {
+//     return new Promise((resolve, reject) => {
+//         if (true) {
+//             setTimeout(() => {
+//                 const serverResponse = {
+//                     status: 200,
+//                     message: 'Todo guay'
+//                 };
+//                 resolve(serverResponse);
+//             }, 3000);
+//             // resolve('Hello!');
+//         } else {
+//             const serverResponse = {
+//                 status: 404,
+//                 message: 'URL not found'
+//             };
+//             reject(serverResponse);
+//         }
+//     });
+// }
+
+
+// photoUpload().then(response => {
+//         console.log(response);
+//         return response.message.toUpperCase();
+//     })
+//     .then(res => {
+//         console.log('Este es mi segundo .then', res);
+//     })
+//     .catch(error => {
+//         console.log('Se ha producido un error', error);
+//     })
+//     .finally(()=>{
+//         console.log('Se ha ejecutado el FINALLY');
+//     });
+
+// console.log('Fin del proceso');
+
+
+//Metodos de las promesas
+const helloPromise = () => {
     return new Promise((resolve, reject) => {
         if (true) {
             setTimeout(() => {
-                const serverResponse = {
-                    status: 200,
-                    message: 'Todo guay'
-                };
-                resolve(serverResponse);
-            }, 3000);
-            // resolve('Hello!');
+                resolve('Hello!');
+            }, 200);
         } else {
-            const serverResponse = {
-                status: 404,
-                message: 'URL not found'
-            };
-            reject(serverResponse);
+            reject('Ouch!!');
         }
     });
-}
+};
 
-
-photoUpload().then(response => {
-        console.log(response);
-        return response.message.toUpperCase();
-    })
-    .then(res => {
-        console.log('Este es mi segundo .then', res);
-    })
-    .catch(error => {
-        console.log('Se ha producido un error', error);
-    })
-    .finally(()=>{
-        console.log('Se ha ejecutado el FINALLY');
+const goodbyePromise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('Goodbye!');
+            }, 3000);
+        } else {
+            reject('Ouch!!');
+        }
     });
+};
 
-console.log('Fin del proceso');
+//ejecuta las dos promesas
+Promise.all([helloPromise(), goodbyePromise()])
+.then(response=> console.log('Respuesta es:', response))//cuando tiene las dos ejecutadas nos muestra el resultado
+.catch(error=>{
+    console.log('Se ha producido un error', error);
+});
