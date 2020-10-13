cb4ae18ed006f9c3658bc21be66302f4c1b362c8
MSI\moneo <jvr.moneo@gmail.com>
2020-10-13 11:51:46 +0200

    sintaxis 3

--- promises.js
+++ promises.js
@@ -40,28 +40,66 @@
 // console.log('Fin del proceso');
 
 //sintaxis 2
+// console.log('Iniciando proceso de subida');
+
+// const photoUpload = new Promise((resolve, reject) => {
+//     if (true) {
+//         setTimeout(() => {
+//             const serverResponse = {
+//                 status: 200,
+//                 message: 'Todo guay'
+//             };
+//             resolve(serverResponse);
+//         }, 3000);
+//         // resolve('Hello!');
+//     } else {
+//         const serverResponse = {
+//             status: 404,
+//             message: 'URL not found'
+//         };
+//         reject(serverResponse);
+//     }
+// });
+
+// photoUpload.then(response => {
+//         console.log(response);
+//         return response.message.toUpperCase();
+//     })
+//     .then(res => {
+//         console.log('Este es mi segundo .then', res);
+//     })
+//     .catch(error => {
+//         console.log('Se ha producido un error', error);
+//     });
+
+// console.log('Fin del proceso');
+
+//sintaxis 3
 console.log('Iniciando proceso de subida');
 
-const photoUpload = new Promise((resolve, reject) => {
-    if (true) {
-        setTimeout(() => {
+function photoUpload() {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                const serverResponse = {
+                    status: 200,
+                    message: 'Todo guay'
+                };
+                resolve(serverResponse);
+            }, 3000);
+            // resolve('Hello!');
+        } else {
             const serverResponse = {
-                status: 200,
-                message: 'Todo guay'
+                status: 404,
+                message: 'URL not found'
             };
-            resolve(serverResponse);
-        }, 3000);
-        // resolve('Hello!');
-    } else {
-        const serverResponse = {
-            status: 404,
-            message: 'URL not found'
-        };
-        reject(serverResponse);
-    }
-});
+            reject(serverResponse);
+        }
+    });
+}
 
-photoUpload.then(response => {
+
+photoUpload().then(response => {
         console.log(response);
         return response.message.toUpperCase();
     })
