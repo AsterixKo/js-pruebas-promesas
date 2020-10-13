d47abcb6eb8407c0811737a1a4e241f2b1572adf
MSI\moneo <jvr.moneo@gmail.com>
2020-10-13 10:25:39 +0200

    Contenido inicial

---
+++ promises.js
@@ -0,0 +1,15 @@
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(()=>{
+                resolve('Hello!');
+            }, 3000);
+            // resolve('Hello!');
+        } else {
+            reject('Ouch!!!');
+        }
+    });
+};
+
+const result = helloPromise();
+console.log('result', result);
